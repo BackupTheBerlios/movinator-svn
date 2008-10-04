362f9cb134b421c1e777df48c4227524133a096a
caladopc <caladopc@1f33cccd-0e54-0410-81b3-be4ab078da4d>
2008-10-04 17:47:41 +0000

    Added the homepage.

---
+++ homepage/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+   <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+   <title>The Movinator</title>
+   <link rel="stylesheet" href="//developer.berlios.de/berlios.css" type="text/css">
+</head>
+<body>
+
+<!-- top strip -->
+<table BORDER=0 CELLSPACING=0 CELLPADDING=2 WIDTH="100%" BGCOLOR="#7B7B7B" >
+<tr>
+<td><span class=maintitlebar>&nbsp; 
+<b><a href="//www.berlios.de/index.php.en" class="maintitlebar">Home</a></b> |
+<b><a href="//developer.berlios.de/about.php" class="maintitlebar">About us</a></b> |
+<b><a href="//developer.berlios.de/partners.php" class="maintitlebar">Partners</a></b> |
+<b><a href="//developer.berlios.de/contact.php" class="maintitlebar">Contact</a></b></span></td>
+</tr>
+</table>
+<!-- end top strip -->
+
+<!-- top title -->
+<table BORDER=0 CELLSPACING=0 CELLPADDING=0 WIDTH="100%" >
+<tr VALIGN=TOP BGCOLOR="#FFCC33">
+<td BGCOLOR="#FFFFFF"><a href="//www.berlios.de/index.php.en"><img src="//developer.berlios.de/images/berliOS_logo.png" ALT="BerliOS" HSPACE=5 VSPACE=5 BORDER=0 height=61 width=238></a></td>
+<td WIDTH="10" BGCOLOR="#FFCC33"><img SRC="//developer.berlios.de/images/blank.gif" ALT="" BORDER=0 height=1 width=10></td>
+<td VALIGN=middle WIDTH="99%"><b><font size="+1">BerliOS</font></b>
+<br>The Open Source Mediator</td>
+
+<!-- logo at right -->
+<td VALIGN=middle>
+<a href="http://www.fokus.fraunhofer.de/" TARGET="_blank"><img SRC="//developer.berlios.de/images/logo_fokus.png" ALT="FOKUS" HSPACE=10 BORDER=0 height=60 width=60 align=RIGHT></a></td>
+</tr>
+<!-- end logo at right -->
+
+<tr>
+<td COLSPAN="4" BGCOLOR="#7B7B7B"><img SRC="//developer.berlios.de/images/blank.gif" ALT="" height=2 width=2></td>
+</tr>
+</table>
+<!-- end top title -->
+
+<!-- ---------------------------------------------------------------------- -->
+<!-- ---------------------------------------------------------------------- -->
+
+<!-- content -->
+
+<APPLET CODE="freemind.main.FreeMindApplet.class"
+        ARCHIVE="freemindbrowser.jar" WIDTH="100%" HEIGHT="100%">
+    <PARAM NAME="type" VALUE="application/x-java-applet;version=1.4">
+    <PARAM NAME="scriptable" VALUE="false">
+    <PARAM NAME="modes" VALUE="freemind.modes.browsemode.BrowseMode">
+    <PARAM NAME="browsemode_initial_map"
+         VALUE="http://movinator.berlios.de/movinator.mm">
+    <!--          ^ Put the path to your map here  -->
+    <param NAME="initial_mode" VALUE="Browse">
+    <param NAME="selection_method" VALUE="selection_method_direct">
+</applet>
+
+<!-- end content -->
+
+<!-- ---------------------------------------------------------------------- -->
+<!-- ---------------------------------------------------------------------- -->
+
+<!-- footer -->
+<table BORDER=0 CELLSPACING=0 CELLPADDING=2 WIDTH="100%" BGCOLOR="#7B7B7B" >
+<tr>
+<td ALIGN=CENTER><span class="titlebar"><font color="#FFFFFF">Copyright
+&copy; 2000-<?php echo date("Y"); ?> <a href="http://www.fokus.fraunhofer.de/" class="maintitlebar" TARGET="_blank">
+FOKUS</a></font></span></td>
+</tr>
+</table>
+<!-- end footer -->
+
+</body>
+</html>
---
+++ homepage/movinator.mm
@@ -0,0 +1,64 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1223139482497" ID="Freemind_Link_1149052339" MODIFIED="1223141215711" TEXT="The Movinator">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1223140923653" FOLDED="true" ID="_" MODIFIED="1223141215444" POSITION="right" TEXT="About">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1223140926479" ID="Freemind_Link_707363057" MODIFIED="1223141215456" TEXT="What it is">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1223140929711" ID="Freemind_Link_264512020" MODIFIED="1223141215487" TEXT="The Movinator is a small database application. It stores information about movies, including critic ratings and the user&#xb4;s rating. Using these it is able to make reccomendations, predicting what would be the user&#xb4;s rating for a yet unseen movie.">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1223141027163" ID="Freemind_Link_189633068" MODIFIED="1223141215560" TEXT="Present and Future">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1223141065857" ID="Freemind_Link_1539337623" MODIFIED="1223141215607" TEXT="The Movinator was made for my personal use and is, therefore, very personalized. I intend to make it more general, but this will be a slow process.">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1223141138814" ID="Freemind_Link_41219930" MODIFIED="1223141215669" TEXT="Of course, the code is public (licensed under GPL) so, feel free to use it and improve it.">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1223141676547" ID="Freemind_Link_411972078" MODIFIED="1223141681246" POSITION="right" TEXT="User Manual">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1223141438956" ID="Freemind_Link_1556327994" LINK="http://developer.berlios.de/projects/movinator" MODIFIED="1223141453160" POSITION="left" TEXT="Project page">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1223141559423" FOLDED="true" ID="Freemind_Link_859550803" MODIFIED="1223141564418" POSITION="left" TEXT="Files">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1223141565055" ID="Freemind_Link_770897112" LINK="http://svn.berlios.de/svnroot/repos/movinator/" MODIFIED="1223141577546" TEXT="Browse SVN">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1223141593029" ID="Freemind_Link_733444883" LINK="http://developer.berlios.de/project/showfiles.php?group_id=9959" MODIFIED="1223141605771" TEXT="Dowloads">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1223141694787" FOLDED="true" ID="Freemind_Link_811679423" MODIFIED="1223141697478" POSITION="left" TEXT="Requirements">
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1223141790904" ID="Freemind_Link_765999290" MODIFIED="1223141792354" TEXT="OS">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1223141734120" ID="Freemind_Link_1794189155" MODIFIED="1223141795152" TEXT="The Movinator is implemented in Python, so it should be able to run on any operating system">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1223141769383" ID="Freemind_Link_1926283595" MODIFIED="1223141797466" TEXT="It was tested on MacOS X and Linux (Debian)">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1223141799310" ID="Freemind_Link_84876278" MODIFIED="1223141800898" TEXT="Libraries">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1223141801622" ID="Freemind_Link_1853925539" MODIFIED="1223142035969" TEXT="SQLite 3 Python module">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1223141911826" ID="Freemind_Link_223101406" MODIFIED="1223141918614" TEXT="WxPython">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1223141668371" ID="Freemind_Link_174797764" MODIFIED="1223141670862" POSITION="right" TEXT="Screenshots">
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+</map>
