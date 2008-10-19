a0503ae789bccae1087d4c923a0984bab411b8fb
caladopc <caladopc@1f33cccd-0e54-0410-81b3-be4ab078da4d>
2008-10-19 15:52:48 +0000

    Added screenshots to the website.

--- homepage/movinator.mm
+++ homepage/movinator.mm
@@ -20,9 +20,42 @@
 <font NAME="SansSerif" SIZE="14"/>
 </node>
 </node>
+<node COLOR="#00b439" CREATED="1224429783786" FOLDED="true" ID="Freemind_Link_179259371" MODIFIED="1224429787645" TEXT="License">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1224429796849" ID="Freemind_Link_1417079218" MODIFIED="1224429886772" TEXT="Movinator is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.  &#xa;&#xa;This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.  &#xa;&#xa;You should have received a copy of the GNU General Public License along with this program.  If not, see &lt;http://www.gnu.org/licenses/&gt;.">
+<font NAME="SansSerif" SIZE="14"/>
 </node>
-<node COLOR="#0033ff" CREATED="1223141676547" ID="Freemind_Link_411972078" MODIFIED="1223141681246" POSITION="right" TEXT="User Manual">
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1223141676547" FOLDED="true" ID="Freemind_Link_411972078" MODIFIED="1223141681246" POSITION="right" TEXT="User Manual">
 <font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1224429410728" ID="Freemind_Link_1182590708" MODIFIED="1224429413866" TEXT="Instalation">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1224429448079" ID="Freemind_Link_368562265" LINK="#Freemind_Link_84876278" MODIFIED="1224429666827" TEXT="Install the required libraries/applications">
+<arrowlink COLOR="#e71414" DESTINATION="Freemind_Link_84876278" ENDARROW="Default" ENDINCLINATION="74;-295;" ID="Freemind_Arrow_Link_1518643708" STARTARROW="None" STARTINCLINATION="-85;-327;"/>
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="full-1"/>
+</node>
+<node COLOR="#990000" CREATED="1224429534371" ID="Freemind_Link_289609341" MODIFIED="1224429669067" TEXT="Unpack the tarball">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="full-2"/>
+<node COLOR="#111111" CREATED="1224429548971" ID="Freemind_Link_276888595" MODIFIED="1224429551414" TEXT="tar -xzvf movinator-v0.1.tgz"/>
+</node>
+<node COLOR="#990000" CREATED="1224429567058" ID="Freemind_Link_1274891964" MODIFIED="1224429671011" TEXT="Create the database">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="full-3"/>
+<node COLOR="#111111" CREATED="1224429574655" ID="Freemind_Link_133749580" MODIFIED="1224429576853" TEXT="sqlite3 moviedb.db &quot;.read createdb.sql&quot;"/>
+</node>
+<node COLOR="#990000" CREATED="1224429585641" ID="Freemind_Link_1321694307" MODIFIED="1224429672971" TEXT="Execute the application">
+<font NAME="SansSerif" SIZE="14"/>
+<icon BUILTIN="full-4"/>
+<node COLOR="#111111" CREATED="1224429592891" ID="Freemind_Link_320792543" MODIFIED="1224429594989" TEXT="./movinator.py"/>
+<node COLOR="#111111" CREATED="1224429640423" ID="Freemind_Link_1570081754" MODIFIED="1224429660521" TEXT=" make sure the file &quot;movinator.py&quot; is executable &#xa;(e.g., by doing chmod u+x movinator.py)"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1224429679934" ID="Freemind_Link_784221220" MODIFIED="1224429681169" TEXT="Usage">
+<font NAME="SansSerif" SIZE="16"/>
+</node>
 </node>
 <node COLOR="#0033ff" CREATED="1223141438956" ID="Freemind_Link_1556327994" LINK="http://developer.berlios.de/projects/movinator" MODIFIED="1223141453160" POSITION="left" TEXT="Project page">
 <font NAME="SansSerif" SIZE="18"/>
@@ -47,7 +80,7 @@
 <font NAME="SansSerif" SIZE="14"/>
 </node>
 </node>
-<node COLOR="#00b439" CREATED="1223141799310" FOLDED="true" ID="Freemind_Link_84876278" MODIFIED="1224416354811" TEXT="Libraries/Applications">
+<node COLOR="#00b439" CREATED="1223141799310" FOLDED="true" ID="Freemind_Link_84876278" MODIFIED="1224429510899" TEXT="Libraries/Applications">
 <font NAME="SansSerif" SIZE="16"/>
 <node COLOR="#990000" CREATED="1224347468467" ID="Freemind_Link_1137239409" LINK="http://www.sqlite.org/" MODIFIED="1224416400834" TEXT="SQLite &gt;= 3.5">
 <font NAME="SansSerif" SIZE="14"/>
@@ -60,8 +93,20 @@
 </node>
 </node>
 </node>
-<node COLOR="#0033ff" CREATED="1223141668371" ID="Freemind_Link_174797764" MODIFIED="1223141670862" POSITION="right" TEXT="Screenshots">
+<node COLOR="#0033ff" CREATED="1223141668371" FOLDED="true" ID="Freemind_Link_174797764" MODIFIED="1223141670862" POSITION="right" TEXT="Screenshots">
 <font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1224431143188" FOLDED="true" ID="Freemind_Link_1758236609" MODIFIED="1224431190351" TEXT="Interface">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1224431191806" ID="Freemind_Link_995409731" MODIFIED="1224431196942" TEXT="&lt;html&gt;&lt;img src=&quot;screen01.png&quot;&gt;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1224431159365" FOLDED="true" ID="Freemind_Link_1355103231" MODIFIED="1224431163560" TEXT="Edited fields">
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1224431210379" ID="Freemind_Link_413542826" MODIFIED="1224431217984" TEXT="&lt;html&gt;&lt;img src=&quot;screen02.png&quot;&gt;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
 </node>
 </node>
 </map>
